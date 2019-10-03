515db18e8f00a3203dca6ba3cae21be758691caa
quy nguyen <chjcovay@gmail.com>
2019-10-03 10:55:19 +0700

    buoi2

---
+++ ex2.js
@@ -0,0 +1,20 @@
+//C1
+class SinhVien {
+    constructor(){
+        this.name = 'Nguyễn Văn Quý';
+    }
+    printsv = () =>{
+    console.log(this.name);
+}
+}
+const sinhVien = new SinhVien();
+//console.log(sinhVien.name);
+sinhVien.printsv();
+//C2
+/*class SinhVien{
+    name = 'Nguyễn Văn Quý';
+}
+
+const sinhVien = new SinhVien();
+console.log(sinhVien.name);
+*/
